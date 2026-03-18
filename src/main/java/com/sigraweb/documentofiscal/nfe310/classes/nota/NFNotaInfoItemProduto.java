0c9439fdbc0697fa8bc26b14babd8bccab2db897
Wesley-1618 <wesleyisilv@gmail.com>
2026-03-18 15:46:28 -0300

    ft: adicionado tags de pedido di item e tag de peso para item

--- src/main/java/com/sigraweb/documentofiscal/nfe310/classes/nota/NFNotaInfoItemProduto.java
+++ src/main/java/com/sigraweb/documentofiscal/nfe310/classes/nota/NFNotaInfoItemProduto.java
@@ -113,6 +113,12 @@ public class NFNotaInfoItemProduto extends DFBase {
 
     @Element(name = "nRECOPI", required = false)
     private String numeroRECOPI;
+    
+    @Element(name = "pesoL", required = false)
+    private String pesoLiquido;
+    
+    @Element(name = "pesoB", required = false)
+    private String pesoBruto;
 
     public void setCodigo(final String codigo) {
         StringValidador.tamanho60(codigo, "Codigo Produto");
@@ -193,6 +199,14 @@ public class NFNotaInfoItemProduto extends DFBase {
 
     public void setValorOutrasDespesasAcessorias(final BigDecimal valorOutrasDespesasAcessorias) {
         this.valorOutrasDespesasAcessorias = BigDecimalParser.tamanho15Com2CasasDecimais(valorOutrasDespesasAcessorias, "Valor Outras Despesas Acessorias Produto");
+    }
+    
+    public void setPesoLiquido(final BigDecimal pesoLiquido) {
+        this.pesoLiquido = BigDecimalParser.tamanho15Com2CasasDecimais(pesoLiquido, "Peso Liquido Produto");
+    }
+    
+    public void setPesoBruto(final BigDecimal pesoBruto) {
+        this.pesoBruto = BigDecimalParser.tamanho15Com2CasasDecimais(pesoBruto, "Peso Bruto Produto");
     }
 
     public void setCampoeValorNota(final NFProdutoCompoeValorNota compoeValorNota) {
@@ -396,4 +410,12 @@ public class NFNotaInfoItemProduto extends DFBase {
     public String getNumeroRECOPI() {
         return this.numeroRECOPI;
     }
+    
+	public String getPesoLiquido() {
+		return this.pesoLiquido;
+	}
+
+	public String getPesoBruto() {
+		return this.pesoBruto;
+	}    
 }
